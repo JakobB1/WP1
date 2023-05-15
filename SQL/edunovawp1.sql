bd8476395c75888da1e1472974aad1465d8cdc47
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2023-05-15 18:24:25 +0200

    insert - clan

--- SQL/edunovawp1.sql
+++ SQL/edunovawp1.sql
@@ -65,3 +65,41 @@ values ('WP1',1,'2023-04-26 17:00:00');
 -- 2
 insert into grupa(naziv,smjer,datumpocetka)
 values ('JP28',2,'2023-04-26 19:00:00');
+
+select * from polaznik;
+
+insert into polaznik(ime,prezime,oib,email)
+values 
+('Luka','Bušić',null,'busic.luka1@gmail.com'),
+('Domagoj','Marković',null,'markovicdomagoj1995@gmail.com'),
+('Andrija','Kruhoberec',null,'akruhoberec1@outlook.com'),
+('Vedran','Džanko',null,'vedran.dzanko@gmail.com'),
+('Antonio','Majić', null, 'antonio.majich@gmail.com'),
+('Lana','Jelenić',null,'lana.jelenic@gmail.com'),
+('Jasenka','Augustinović',null,'jaugustinovic85@gmail.com'),
+('Domagoj','Ljubičić',null,'dljubicic2@gmail.com'),
+('Leo','Lovenjak',null,'leo.lovenjak107@gmail.com'),
+('Leon','Bičak',null,'lbicak96@gmail.com'),
+('Katarina','Pavličević',null,'katarina.pavlicevic5@gmail.com'),
+('Darko','Kučan',null,'dkucan61@gmail.com'),
+('Patrik','Gomerčić',null,'patrik.gomercic3@gmail.com'),
+('Antonio','Šubarić',null,'antonio.subaric98@gmail.com'),
+('Srđan','Filipović',null,'srdjanfilipovic991@gmail.com'),
+('Jakob','Brkić',null,'jakobbrkic97@gmail.com'),
+('Alen','Oroz',null,'alen.oroz1@gmail.com'),
+('Ivor','Ćelić',null,'ivorcelic@gmail.com'),
+('Anja','Andonovski',null,'andonovski.anja@gmail.com'),
+('Bruno','Bušić',null,'brunobusic20@gmail.com'),
+('Karlo','Perić',null,'karlocar25@gmail.com'),
+('Kristijan','Beriša',null,'k_berisa@hotmail.com'),
+('Valentin','Mijatović',null,'mijatovic.valentin123@gmail.com'),
+('Ivan','Angebrandt',null,'ivan.angebrandt@gmail.com'),
+('Luka','Mrđa',null,'luka.mrda@yahoo.com');
+
+select * from clan;
+
+insert into clan(grupa,polaznik)
+values (1,1),(1,2),(1,3),(1,4),(1,5),(1,6),
+(1,7),(1,8),(1,9),(1,10),(1,11),(1,12),(1,13),(1,14),
+(1,15),(1,16),(1,17),(1,18),(1,19),(1,20),
+(1,21),(1,22),(1,23),(1,24),(1,25);
--- SQL/insertwp1.sql
+++ SQL/insertwp1.sql
@@ -1,10 +1,3 @@
 ﻿use edunovawp1;
 use master;
 
-select * from grupa;
--- 1
-insert into grupa(naziv,smjer,datumpocetka)
-values ('WP1',1,'2023-04-26 17:00:00');
--- 2
-insert into grupa(naziv,smjer,datumpocetka)
-values ('JP28',2,'2023-04-26 19:00:00');
