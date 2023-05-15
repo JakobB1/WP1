573b833d0e657d166bcb6e69574cb685c4def13e
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2023-05-15 18:48:30 +0200

    Vjezbanje inserta(samouprava)

--- SQL/edunovawp1.sql
+++ SQL/edunovawp1.sql
@@ -103,3 +103,27 @@ values (1,1),(1,2),(1,3),(1,4),(1,5),(1,6),
 (1,7),(1,8),(1,9),(1,10),(1,11),(1,12),(1,13),(1,14),
 (1,15),(1,16),(1,17),(1,18),(1,19),(1,20),
 (1,21),(1,22),(1,23),(1,24),(1,25);
+
+
+
+
+-- vjezbanje:
+-- 1. Unijeti smjer PHP programiranje 
+-- 2. Na PHP programiranje definirati dvije grupa (PP17 i PP17)
+-- 3. Na PP18 postaviti 3 polaznika (koje ste prethodno unijeli)
+
+-- 1. zadatak
+select * from smjer;
+insert into smjer (naziv,trajanje,cijena,upisnina,verificiran)
+values ('PHP programiranje',160,1999.99,49.99,0);
+
+-- 2.zadatak
+select * from grupa;
+insert into grupa(naziv,smjer,datumpocetka)
+values ('PP17',4,'2023-04-27 17:00:00');
+insert into grupa(naziv,smjer,datumpocetka)
+values ('PP18',4,'2023-04-27 19:00:00');
+
+-- 3.zadatak
+select * from smjer;
+
--- SQL/samoupravawp1.sql
+++ SQL/samoupravawp1.sql
@@ -1,4 +1,4 @@
-use master;
+﻿use master;
 drop database if exists samoupravawp1;
 go
 create database samoupravawp1;
@@ -32,3 +32,42 @@ create table mjesto(
 alter table zupanija add foreign key (zupan) references zupan(sifra);
 alter table opcina add foreign key (zupanija) references zupanija(sifra);
 alter table mjesto add foreign key (opcina) references opcina(sifra);
+
+
+select * from zupan;
+insert into zupan(ime,prezime)
+values ('Pero','Perić'),
+	   ('Marko','Marković'),
+	   ('Ivo','Ivić');
+
+
+select * from zupanija;
+insert into zupanija(naziv,zupan)
+values ('Zupanija1',1),
+	   ('Zupanija2',2),
+	   ('Zupanija3',3);
+
+
+select * from opcina;
+insert into opcina(zupanija,naziv)
+values (1,'Opcina1'),
+	   (1,'Opcina2'),
+	   (2,'Opcina3'),
+	   (2,'Opcina4'),
+	   (3,'Opcina5'),
+	   (3,'Opcina6');
+
+select * from mjesto;
+insert into mjesto(opcina,naziv)
+values (1,'Mjesto1'),
+	   (1,'Mjesto2'),
+	   (1,'Mjesto3'),
+	   (1,'Mjesto4'),
+	   (2,'Mjesto5'),
+	   (2,'Mjesto6'),
+	   (2,'Mjesto7'),
+	   (2,'Mjesto8'),
+	   (3,'Mjesto9'),
+	   (3,'Mjesto10'),
+	   (3,'Mjesto11'),
+	   (3,'Mjesto12');
