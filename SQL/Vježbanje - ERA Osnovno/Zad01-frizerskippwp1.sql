ff73153bf87dfe39602fab6c9b083c6268d2d6d7
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2023-05-16 15:53:30 +0200

    Uvod - alter table i insert naredbe

--- SQL/Vježbanje - ERA Osnovno/Zad01-frizerskippwp1.sql
+++ SQL/Vježbanje - ERA Osnovno/Zad01-frizerskippwp1.sql
@@ -30,3 +30,47 @@ create table usluga (
     naziv varchar(50) not null,
     cijena decimal(18,2)
 );
+
+
+
+alter table salon add foreign key (djelatnica) references djelatnica (sifra);
+
+alter table djelatnica add foreign key (korisnik) references korisnik (sifra);
+
+alter table korisnik add foreign key (usluga) references usluga (sifra);
+
+
+
+select * from usluga;
+insert into usluga(naziv,cijena)
+values ('Usluga01',99.99),
+       ('Usluga02',199.99),
+       ('Usluga03',299.99),
+       ('Usluga04',399.99),
+       ('Usluga05',499.99);
+
+select * from korisnik;
+insert into korisnik(ime,email,usluga)
+values ('Korisnik01','mail01@mail.com',4),
+       ('Korisnik02','mail02@mail.com',2),
+       ('Korisnik03','mail03@mail.com',5),
+       ('Korisnik04','mail04@mail.com',2),
+       ('Korisnik05','mail05@mail.com',1);
+
+
+select * from djelatnica;
+insert into djelatnica(ime,iban,korisnik)
+values ('Djelatnica01',12345678911,3),
+       ('Djelatnica02',12345678911,1),
+       ('Djelatnica03',12345678911,2),
+       ('Djelatnica04',12345678911,5),
+       ('Djelatnica05',12345678911,4);
+
+
+select * from salon;
+insert into salon(naziv,djelatnica)
+values ('Salon01',5),
+       ('Salon02',3),
+       ('Salon03',1),
+       ('Salon04',2),
+       ('Salon05',4);
