0e0278601ad50b0bb314fc35e8891291899c60a7
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2023-05-17 15:09:15 +0200

    create table

--- SQL/Vježbanje - ERA Osnovno/Zad10-doktorwp1.sql
+++ SQL/Vježbanje - ERA Osnovno/Zad10-doktorwp1.sql
@@ -5,3 +5,29 @@ create database doktorwp1;
 go
 use doktorwp1;
 
+create table doktor (
+    sifra int not null primary key identity(1,1),
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    pacijent int not null,
+    sestra int not null
+);
+
+create table pacijent (
+    sifra int not null primary key identity(1,1),
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    lijecenje int not null
+);
+
+create table sestra (
+    sifra int not null primary key identity(1,1),
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    doktor int not null
+);
+
+create table lijecenje (
+    sifra int not null primary key identity(1,1),
+    nazivTerapije varchar(50) not null
+); 
