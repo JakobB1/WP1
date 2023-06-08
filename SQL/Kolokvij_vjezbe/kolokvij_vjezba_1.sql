d15d2696bbcbb265de3a27535a38829a1a462870
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2023-06-08 17:01:54 +0200

    Create table - nastaviti

--- SQL/Kolokvij_vjezbe/kolokvij_vjezba_1.sql
+++ SQL/Kolokvij_vjezbe/kolokvij_vjezba_1.sql
@@ -15,3 +15,42 @@ create table mladic(
 	dukserica varchar(48) not null,
 	muskarac int
 );
+
+create table svekar(
+	sifra int not null primary key identity(1,1),
+	bojaociju varchar(40) not null,
+	prstena int,
+	dukserica varchar(41),
+	lipa decimal(13,8),
+	eura decimal(12,7),
+	majica varchar(35)
+);
+
+create table sestra(
+	sifra int not null primary key identity(1,1),
+	introvertno bit,
+	haljina varchar(31) not null,
+	maraka decimal(16,6),
+	hlace varchar(46) not null,
+	narukvica int not null
+);
+
+create table zena(
+	sifra int not null primary key identity(1,1),
+	treciputa datetime,
+	hlace varchar(46),
+	kratkamajica varchar(31) not null,
+	jmbag char(11) not null,
+	bojaociju varchar(39) not null,
+	haljina varchar(44),
+	sestra int not null
+);
+
+create table muskarac(
+	sifra int not null primary key identity(1,1),
+	bojaociju varchar(50) not null,
+	hlace varchar(30),
+	modelnaocala varchar(43),
+	maraka decimal(14,5) not null,
+	zena int not null
+);
