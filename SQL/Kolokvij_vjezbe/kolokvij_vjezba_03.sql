985925420be8cb10b0fbe9667d82d45862ec2558
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2023-06-12 13:34:18 +0200

    vjezba 3 - create table

---
+++ SQL/Kolokvij_vjezbe/kolokvij_vjezba_03.sql
@@ -0,0 +1,74 @@
+use master;
+drop database if exists kolokvij_vjezba_03;
+go
+create database kolokvij_vjezba_03;
+go
+use kolokvij_vjezba_03;
+
+create table cura(
+	sifra int not null primary key identity(1,1),
+	dukserica varchar(49),
+	maraka decimal(13,7),
+	drugiputa datetime,
+	majica varchar(49),
+	novcica decimal(15,8),
+	ogrlica int not null
+);
+
+create table svekar(
+	sifra int not null primary key identity(1,1),
+	novcica decimal(16,8) not null,
+	suknja varchar(44) not null,
+	bojakose varchar(36),
+	prstena int,
+	narukvica int not null,
+	cura int not null
+);
+
+create table brat(
+	sifra int not null primary key identity(1,1),
+	jmbag char(11),
+	ogrlica int not null,
+	ekstrovertno bit not null
+);
+
+create table prijatelj(
+	sifra int not null primary key identity(1,1),
+	kuna decimal(16,10),
+	haljina varchar(37),
+	lipa decimal(13,10),
+	dukserica varchar(31),
+	indiferentno bit not null
+);
+
+create table prijatelj_brat(
+	sifra int not null primary key identity(1,1),
+	prijatelj int not null,
+	brat int not null
+);
+
+create table ostavljena(
+	sifra int not null primary key identity(1,1),
+	kuna decimal(17,5),
+	lipa decimal(15,6),
+	majica varchar(31) not null,
+	prijatelj int 
+);
+
+create table snasa(
+	sifra int not null primary key identity(1,1),
+	introvertno bit,
+	kuna decimal(15,6) not null,
+	eura decimal(12,9) not null,
+	treciputa datetime,
+	ostavljena int not null
+);
+
+create table punica(
+	sifra int not null primary key identity(1,1),
+	asocijalno bit,
+	kratkamajica varchar(44),
+	kuna decimal(13,8) not null,
+	vesta varchar(32) not null,
+	snasa int
+);
