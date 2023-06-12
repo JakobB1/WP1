3e917ba4fa37ed603f0f5208f323dfc77cc1efaa
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2023-06-12 15:38:50 +0200

    vjezba 8 - create table

--- SQL/Kolokvij_vjezbe/kolokvij_vjezba_08.sql
+++ SQL/Kolokvij_vjezbe/kolokvij_vjezba_08.sql
@@ -12,3 +12,61 @@ create table prijateljica(
 	introvertno bit not null
 );
 
+create table cura(
+	sifra int not null primary key identity(1,1),
+	nausnica int not null,
+	indiferentno bit,
+	ogrlica int not null,
+	gustoca decimal(12,6),
+	drugiputa datetime,
+	vesta varchar(33),
+	prijateljica int
+);
+
+create table decko(
+	sifra int not null primary key identity(1,1),
+	kuna decimal(12,10),
+	lipa decimal(17,10),
+	bojakose varchar(44),
+	treciputa datetime not null,
+	ogrlica int not null,
+	ekstrovertno bit not null
+);
+
+create table muskarac(
+	sifra int not null primary key identity(1,1),
+	haljina varchar(47),
+	drugiputa datetime not null,
+	treciputa datetime
+);
+
+create table muskarac_decko(
+	sifra int not null primary key identity(1,1),
+	muskarac int not null,
+	decko int not null
+);
+
+create table becar(
+	sifra int not null primary key identity(1,1),
+	eura decimal(15,10) not null,
+	treciputa datetime,
+	prviputa datetime,
+	muskarac int not null
+);
+
+create table neprijatelj(
+	sifra int not null primary key identity(1,1),
+	kratkamajica varchar(44),
+	introvertno bit,
+	indiferentno bit,
+	ogrlica int not null,
+	becar int not null
+);
+
+create table brat(
+	sifra int not null primary key identity(1,1),
+	introvertno bit,
+	novcica decimal(14,7) not null,
+	treciputa datetime,
+	neprijatelj int
+);
