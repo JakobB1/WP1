9a814461a03e6ef5c869854c7dd80e0a66832647
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2023-06-14 16:00:46 +0200

    Predavanje do 12.6.

---
+++ CHSARP/Console01/ConsoleApp01/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tomislav");
+            Console.WriteLine("Edunova");
+        }
+    }
+}
---
+++ CHSARP/Console01/ConsoleApp03/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("|\\---/|\r\n| o_o |\r\n \\_^_/");
+            //Console.WriteLine(args[0]);
+        }
+    }
+}
