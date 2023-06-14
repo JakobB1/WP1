6b2cb535ea29722ad7bb34546c859205c5a28de3
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2023-06-14 18:48:39 +0200

    Predavanje - 14.6.

---
+++ CHSARP/Console02/E01Varijable/Program.cs
@@ -0,0 +1,67 @@
+﻿
+// Komentar jedna linija
+
+/*
+ *
+ * Komentar više linija
+ */
+
+/*
+ 
+ Komentar 
+ više linija
+ 
+ */
+
+// deklaracija varijable
+int i;
+
+//dodjeljivanje vrijednosti
+i = 4;
+
+
+//korištenje varijable 
+Console.WriteLine(i);
+
+string nizZnakova = "Edunova";
+
+bool logickaVrijednost=true;
+
+Console.WriteLine(logickaVrijednost);
+
+float decimalniBroj = 3.14f;
+double decimalniBrojVeci = 3.14;
+decimal db = 3.14m;
+
+Console.WriteLine("decimalni broj: {0}\ndb: {1}",
+    decimalniBroj, db);
+
+
+Console.WriteLine(int.MinValue);
+Console.WriteLine(int.MaxValue);
+
+int b = int.MaxValue;
+
+b = b + 1;
+
+Console.WriteLine(b);
+
+// Zadatak 1 
+// Deklarirajte 4 varijable različith tipova podataka
+// Svakom od njih dodjelite vrijednost po želji
+// Sve ih odjednom ispišite jedno pored drugog
+
+string zadatak01 = "TestName";
+bool zadatak02 = true;
+float zadatak03 = 23.53f;
+double zadatak04 = 4563.52;
+
+
+// tips
+
+int a, q, w = 4;
+
+bool istina = w == 3;
+
+
+Console.WriteLine(istina);  
---
+++ CHSARP/Console02/E04Operatori/Program.cs
@@ -0,0 +1,48 @@
+﻿
+// operatori uspoređivanja -> bool
+
+bool razlicito = 3 != 4;
+
+
+bool vece = 8>6;
+
+Console.WriteLine("{0},{1}", razlicito, vece);
+
+// Logički operatori -> učimo kod if naredbe
+
+bool rez = razlicito & vece;
+
+// operator + ima dvojaku ulogu
+
+string s = "Edunova" + " Osijek"; //nadoljepiti
+
+int x = 6 + 2; // zbrojiti
+
+string s1 = "Broj " + 5;
+
+// operator modulo ostatak nakon cjelobrojnog djeljenja
+// školski primjer je parni i neparni broj
+
+// Za unesi broj ispiši True ako je parni ili False ako je neparni
+
+Console.WriteLine("Unesi broj: ");
+x = Int16.Parse(Console.ReadLine());    
+
+Console.WriteLine(x%2==0);
+
+
+// Deklarirajte varijablu tipa int naziva negativniBroj i 
+// dodjelite joj vrijednost -262
+int negativniBroj = -262;
+Console.WriteLine(negativniBroj);
+// ispišite pozitivni ekvivalent negativniBroj
+
+
+
+// Za unesena dva cijela broj
+// ispišite razultat djeljenja
+// npr. ulaz 5 i 10, izlaz 0,5
+
+
+
+
