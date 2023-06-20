de23f8d02fdae30c0be54ac99209eb4e416987fd
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2023-06-20 12:49:14 +0200

    vjezba08 - svi zadaci

--- SQL/Kolokvij_vjezbe/kolokvij_vjezba_08.sql
+++ SQL/Kolokvij_vjezbe/kolokvij_vjezba_08.sql
@@ -78,3 +78,85 @@ alter table neprijatelj add foreign key(becar) references becar(sifra);
 alter table becar add foreign key(muskarac) references muskarac(sifra);
 alter table muskarac_decko add foreign key(muskarac) references muskarac(sifra);
 alter table muskarac_decko add foreign key(decko) references decko(sifra);
+
+--1. U tablice neprijatelj, becar i muskarac_decko unesite po 3 retka.
+select * from muskarac; 
+insert into muskarac (drugiputa)
+values 
+('2017-11-19'),
+('2019-11-19'),
+('2011-11-19');
+     
+select * from becar;
+insert into becar(eura,muskarac)
+values 
+(22.22,1),
+(33.33,2),
+(343.50,1);
+
+select * from neprijatelj;
+insert into neprijatelj(ogrlica,becar)
+values  
+(2,1),
+(7,2),
+(9,3);
+               
+select * from decko;
+insert into decko(treciputa,ogrlica,ekstrovertno)
+values  
+('2013-03-12',6,0),
+('2015-12-21',5,1),
+('2018-11-30',2,1);
+               
+select * from muskarac_decko;
+insert into muskarac_decko(muskarac,decko)
+values  
+(1,3),
+(2,2),
+(3,1);
+
+--2. U tablici cura postavite svim zapisima kolonu indiferentno na vrijednost false.
+select * from prijateljica;  
+insert into prijateljica(nausnica,introvertno)
+values  
+(5,0),
+(3,0),
+(7,1);
+
+select * from cura;
+insert into cura(nausnica,ogrlica,prijateljica)
+values  
+(3,5,1),
+(1,4,2),
+(6,1,3);
+        
+update cura set indiferentno=0;
+
+--3. U tablici brat obrišite sve zapise čija je vrijednost kolone novcica različito od 12,75.
+select * from brat;
+delete from brat where novcica!='12.75';
+
+--4. Izlistajte prviputa iz tablice becar uz uvjet da vrijednost kolone treciputa nepoznate.
+select prviputa from becar where treciputa is null;
+
+--5. 
+--Prikažite bojakose iz tablice decko, neprijatelj iz tablice brat te introvertno iz tablice neprijatelj 
+
+--uz uvjet da su vrijednosti kolone treciputa iz tablice becar poznate 
+--te da su vrijednosti kolone drugiputa iz tablice muskarac poznate. 
+--Podatke posložite po introvertno iz tablice neprijatelj silazno.
+select a.bojakose , f.neprijatelj , e.introvertno 
+from decko a
+inner join muskarac_decko  b on a.sifra      = b.decko
+inner join muskarac        c on b.muskarac   = c.sifra
+inner join becar           d on c.sifra      = d.muskarac 
+inner join neprijatelj     e on d.sifra      = e.becar
+inner join brat            f on e.sifra      = f.neprijatelj 
+where d.treciputa is not null and c.drugiputa is not null
+order by e.introvertno desc;
+
+--6. Prikažite kolone drugiputa i treciputa iz tablice muskarac 
+--čiji se primarni ključ ne nalaze u tablici muskarac_decko.
+select	a.drugiputa , a.treciputa 
+from	muskarac a left join muskarac_decko b on b.muskarac = a.sifra 
+where	b.muskarac is null;
