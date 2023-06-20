3f12fc8fc2d4f3f7ed8f5a9b68a98abd520c3a21
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2023-06-20 15:22:20 +0200

    vjezba13 - svi zadaci

--- SQL/Kolokvij_vjezbe/kolokvij_vjezba_13.sql
+++ SQL/Kolokvij_vjezbe/kolokvij_vjezba_13.sql
@@ -84,3 +84,70 @@ alter table mladic add foreign key(ostavljen) references ostavljen(sifra);
 alter table ostavljen add foreign key(svekrva) references svekrva(sifra);
 alter table svekrva_svekar add foreign key(svekrva) references svekrva(sifra);
 alter table svekrva_svekar add foreign key(svekar) references svekar(sifra);
+
+--1. U tablice mladic, ostavljen i svekrva_svekar unesite po 3 retka.
+select * from svekrva;
+insert into svekrva(ekstrovertno,narukvica)
+values  
+(1,3),
+(0,7),
+(0,2);
+       
+select * from ostavljen;
+insert into ostavljen(asocijalno,svekrva)
+values  
+(0,1),
+(1,2),
+(0,3);
+        
+select * from mladic;
+insert into mladic(drugiputa,carape,kratkamajica,introvertno,asocijalno,ostavljen)  
+values  
+('2013-04-12',5,4,1,0,1),
+('2013-04-12',5,4,1,0,2),
+('2013-04-12',5,4,1,0,3);
+        
+select * from svekar;
+insert into svekar(stilfrizura,gustoca,carape)
+values  
+('Kratka',11,3),
+('Kratka',12,2),
+('Kratka',13,4);
+
+select * from svekrva_svekar;
+insert into svekrva_svekar(svekrva,svekar)
+values  
+(1,3),
+(1,3),
+(1,3);
+
+--2. U tablici neprijatelj postavite svim zapisima kolonu novcica na vrijednost 13,77.
+update neprijatelj set novcica=13.77;  
+
+--3. U tablici cura obrišite sve zapise čija je vrijednost kolone hlace različito od AB.
+delete from cura where hlace like '%AB%';
+
+--4. Izlistajte asocijalno iz tablice ostavljen uz uvjet da vrijednost kolone drugiputa nepoznate.
+select asocijalno from ostavljen where drugiputa is null;
+
+--5.
+--5. Prikažite gustoca iz tablice svekar, majica iz tablice cura te carape iz tablice mladic 
+
+--uz uvjet da su vrijednosti kolone drugiputa iz tablice ostavljen poznate 
+--te da su vrijednosti kolone ogrlica iz tablice svekrva jednake broju 193. 
+--Podatke posložite po carape iz tablice mladic silazno.
+select a.gustoca , f.majica , e.carape 
+from svekar a
+inner join svekrva_svekar b on a.sifra = b.svekar 
+inner join svekrva c on b.svekrva = c.sifra 
+inner join ostavljen d on c.sifra=d.svekrva 
+inner join mladic e on d.sifra=e.ostavljen
+inner join cura f on e.sifra= f.mladic
+where d.drugiputa is not null and c.ogrlica =193
+order by e.carape desc;
+
+--6. Prikažite kolone ogrlica i ekstroventno iz tablice svekrva 
+--čiji se primarni ključ ne nalaze u tablici svekrva_svekar.
+select	a.ogrlica , a.ekstrovertno 
+from	svekrva a left join svekrva_svekar b on a.sifra=b.svekrva
+where	b.svekrva is null;
