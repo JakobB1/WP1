af5b4741ee2f8861270e9ab04bde1a7cb7c17bd6
Jakob Brkić <jale.pnv@gmail.com>
2023-07-13 14:12:29 +0200

    Pomocno

--- CSHARP/Console08/LjetniRad/Pomocno.cs
+++ CSHARP/Console08/LjetniRad/Pomocno.cs
@@ -54,6 +54,34 @@ namespace LjetniRad
             }
         }
 
+        internal static decimal ucitajDecimalniBroj(string poruka,string greska)
+        {
+            decimal b;
+            while (true)
+            {
+                Console.Write(poruka);
+                try
+                {
+                    b = decimal.Parse(Console.ReadLine());
+                    if (b > 0)
+                    {
+                        return b;
+                    }
+                    Console.WriteLine(greska);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(greska);
+                }
+            }
+        }
+
+        internal static bool ucitajBool(string poruka)
+        {
+            Console.Write(poruka);
+            return Console.ReadLine().Trim().ToLower().Equals("da") ? true : false;
+        }
+
         internal static string UcitajString(string poruka, string greska)
         {
             string s = "";
@@ -68,5 +96,20 @@ namespace LjetniRad
                 Console.WriteLine(greska);
             }
         }
+
+        internal static DateTime ucitajDatum(string v1, string v2)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine(v1);
+                    return DateTime.Parse(Console.ReadLine());
+                }catch (Exception ex)
+                {
+                    Console.WriteLine(v2);  
+                }
+            }
+        }
     }
-}
+
