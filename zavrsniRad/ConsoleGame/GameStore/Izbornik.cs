32d5248436b982f959d344a6c6a03bade5fa9987
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2023-08-20 23:44:15 +0200

    ObradaIzdavac i Izbornik

--- zavrsniRad/ConsoleGame/GameStore/Izbornik.cs
+++ zavrsniRad/ConsoleGame/GameStore/Izbornik.cs
@@ -8,9 +8,13 @@ namespace GameStore
 {
     internal class Izbornik
     {
+        public ObradaIzdavac ObradaIzdavac { get;}
+
         public Izbornik() 
         {
+            ObradaIzdavac = new ObradaIzdavac();
             PozdravnaPoruka();
+            PrikaziIzbornik();  
         }
 
         private void PozdravnaPoruka()
@@ -18,6 +22,37 @@ namespace GameStore
             Console.WriteLine("***************************************");
             Console.WriteLine("***** GameStore Console APP v 1.0 *****");
             Console.WriteLine("***************************************");
+        }
+
+        private void PrikaziIzbornik()
+        {
+            Console.WriteLine("Glavni izbornik");
+            Console.WriteLine("1. Izdavac");
+            Console.WriteLine("2. Igra");
+            Console.WriteLine("3. Korisnik");
+            Console.WriteLine("4. Narudzba");
+            Console.WriteLine("5. Izlaz iz programa");
+
+            switch(Pomocno.ucitajBrojRaspon("Odaberite stavku izbornika: ",
+                "Odabir mora biti 1-5", 1, 5))
+            {
+                case 1:
+                    ObradaIzdavac.PrikaziIzbornik();
+                    PrikaziIzbornik();
+                    break;
+                case 2:
+                    PrikaziIzbornik();
+                    break;
+                case 3:
+                    PrikaziIzbornik();
+                    break;
+                case 4:
+                    PrikaziIzbornik();
+                    break;
+                case 5:
+                    Console.WriteLine("Hvala na koristenju, dovidenja");
+                    break;  
+            }
         }
     }
 }
--- zavrsniRad/ConsoleGame/GameStore/ObradaIzdavac.cs
+++ zavrsniRad/ConsoleGame/GameStore/ObradaIzdavac.cs
@@ -8,5 +8,110 @@ namespace GameStore
 {
     internal class ObradaIzdavac
     {
+        public List<Izdavac> Izdavaci { get; }
+
+        public ObradaIzdavac()
+        {
+            Izdavaci = new List<Izdavac>();
+            if (Pomocno.dev)
+            {
+                TestniPodaci();
+            }
+        }
+
+        public void PrikaziIzbornik()
+        {
+            Console.WriteLine("Izbornik za rad s izdavacima");
+            Console.WriteLine("1. Pregled postojecih izdavaca");
+            Console.WriteLine("2. Unos novog izdavaca");
+            Console.WriteLine("3. Promjena postojecih izdavaca");
+            Console.WriteLine("4. Brisanje izdavaca");
+            Console.WriteLine("5. Povratak na glavni izbornik");
+            
+            switch(Pomocno.ucitajBrojRaspon("Odabreite stavku izbornika izdavaca: ",
+                "Odabir mora biti 1-5", 1, 5))
+            {
+                case 1:
+                    PrikaziIzdavace();
+                    PrikaziIzbornik();
+                    break;
+                case 2:
+                    UnosNovogIzdavaca();
+                    PrikaziIzbornik();
+                    break;
+                case 3:
+                    PromjenaIzdavaca();
+                    PrikaziIzbornik();
+                    break;
+                case 4:
+                    BrisanjeIzdavaca();
+                    PrikaziIzbornik();  
+                    break;
+                case 5:
+                    Console.WriteLine("Gotov rad s izdavacima");
+                    break;
+            }
+        }
+
+        public void PromjenaIzdavaca()
+        {
+            PrikaziIzdavace();
+            int index = Pomocno.ucitajBrojRaspon("Odaberi redni broj izdavaca: ", "Nije dobar odabir", 1, Izdavaci.Count());
+            var i = Izdavaci[index - 1];
+            i.Sifra = Pomocno.ucitajCijeliBroj("Unesite sifru izdavaca (" + i.Sifra + "): ",
+                "Unos mora biti pozitivni cijeli broj");
+            i.Naziv = Pomocno.UcitajString("Unesite naziv izdavaca (" + i.Naziv + "): ",
+                "Unos obavezan");
+            i.Drzava = Pomocno.UcitajString("Unesite drzavu izdavaca (" + i.Naziv + "): ",
+                "Unos obavezan");
+            i.WebStranica = Pomocno.UcitajString("Unesite web stranicu izdavaca (" + i.Naziv + "): ",
+                "Unos obavezan");
+        }
+
+        private void BrisanjeIzdavaca()
+        {
+            PrikaziIzdavace();
+            int index = Pomocno.ucitajBrojRaspon("Odaberi redni broj izdavaca: ", "Nije dobar odabir", 1, Izdavaci.Count());
+            Izdavaci.RemoveAt(index - 1);
+        }
+
+
+        private void UnosNovogIzdavaca()
+        {
+            var i = new Izdavac();
+            i.Sifra = Pomocno.ucitajCijeliBroj("Unesite sifru izdavaca: ",
+                "Unos mora biti pozitivni cijeli broj");
+            i.Naziv = Pomocno.UcitajString("Unesite naziv izdavaca: ",
+                "Unos obavezan");
+            i.Drzava = Pomocno.UcitajString("Unesite drzavu izdavaca: ",
+                "Unos obavezan");
+            i.WebStranica = Pomocno.UcitajString("Unesite web stranicu izdavaca: ",
+                "Unos obavezan");
+            Izdavaci.Add(i);
+        }
+
+        public void PrikaziIzdavace()
+        {
+            Console.WriteLine("------------------");
+            Console.WriteLine("---- Izdavaci ----");
+            Console.WriteLine("------------------");
+            int b = 1;
+            foreach (Izdavac izdavac in Izdavaci)
+            {
+                Console.WriteLine("{0}. {1}", b++, izdavac.Naziv);
+            }
+            Console.WriteLine("------------------");
+        }
+
+        private void TestniPodaci()
+        {
+            Izdavaci.Add(new Izdavac
+            {
+                Sifra = 1,
+                Naziv = "Izdavac01",
+                Drzava = "Drzava01",
+                WebStranica = "Web01"
+            });
+        }
     }
 }
