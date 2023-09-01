1bb4175ff359accac275c33b5161a88292046b3e
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2023-09-01 17:15:17 +0200

    Predavanje - 30.08.

--- CSHARP/EdunovaWebAPI/HelloWorld/Controllers/HelloWorldController.cs
+++ CSHARP/EdunovaWebAPI/HelloWorld/Controllers/HelloWorldController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace HelloWorld.Controllers
 {
@@ -55,8 +56,97 @@ namespace HelloWorld.Controllers
         // cikličnu matricu kao dvodimenzionalni niz brojeva
 
 
+        [HttpGet]
+        [Route("{sifra:int}")]
+        public string PozdravRuta(int sifra)
+        {
+            return "Hello " + sifra;
+        }
+
+        [HttpGet]
+        [Route("{sifra:int}/{kategorija}")]
+        public string PozdravRuta2(int sifra, string kategorija)
+        {
+            return "Hello " + sifra + " " + kategorija;
+        }
+
+        [HttpPost]
+        public string DodavanjeNovog(string ime)
+        {
+            return "Dodao " + ime;
+        }
+
+        [HttpPut]
+        public string Promjena(int sifra, string naziv)
+        {
+            return "Na šifri " + sifra + " postavljam " + naziv;   
+        }
+
+        [HttpDelete]
+        public bool Obrisao(int sifra)
+        {
+            return true;
+        }
 
 
 
+
+
+
+        [HttpGet]
+        [Route("matrica")]
+        public IActionResult Matrica(int redaka, int stupaca)
+        {
+           // moj kod koji to napuni
+
+            int[,] matrica = new int[redaka, stupaca];
+
+            int b = 1;
+
+            int n = 0;
+
+            var nizRedaka = new string[redaka];
+
+
+
+            while (b < stupaca * redaka)
+            {
+
+                for (int i = n + 1; i <= stupaca - n; i++)      // s desna na lijevo
+                {
+                    if (b <= stupaca * redaka)
+                        matrica[redaka - n - 1, stupaca - i] = b++;
+                    else break;
+                }
+
+                for (int i = redaka - n - 2; i >= n; i--)          // gore
+                {
+                    if (b <= stupaca * redaka)
+                        matrica[i, n] = b++;
+                    else break;
+                }
+
+
+                for (int i = n + 1; i <= stupaca - n - 1; i++)    // s lijeva na desno
+                {
+                    if (b <= stupaca * redaka)
+                        matrica[n, i] = b++;
+                    else break;
+                }
+
+
+                for (int i = n + 1; i <= redaka - n - 2; i++)       // dolje
+                {
+                    if (b <= stupaca * redaka)
+                        matrica[i, stupaca - n - 1] = b++;
+                    else break;
+                }
+
+                n++;
+
+            }
+
+            return new JsonResult(JsonConvert.SerializeObject(matrica));
+        }
     }
 }
