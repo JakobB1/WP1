7d2f5ca0b784bca5472aeaaaea2107c046e41f47
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2023-09-15 13:24:00 +0200

    Izrada IzdavacController i GET metoda

---
+++ CSHARP/GameApp/GameStoreWebAPI/Controllers/KorisnikController.cs
@@ -0,0 +1,45 @@
+﻿using GameStoreWebAPI.Data;
+using GameStoreWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameStoreWebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class KorisnikController : ControllerBase
+    {
+
+        private readonly GameContext _context;
+
+        public KorisnikController(GameContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var korisnici = _context.Korisnik.ToList();
+                if (korisnici == null || korisnici.Count == 0)
+                {
+                    return new EmptyResult();
+                }
+                return new JsonResult(_context.Korisnik.ToList());
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status503ServiceUnavailable,
+                                    ex.Message);
+            }
+        }
+
+    }
+}
--- CSHARP/GameApp/GameStoreWebAPI/Data/GameContext.cs
+++ CSHARP/GameApp/GameStoreWebAPI/Data/GameContext.cs
@@ -11,5 +11,6 @@ namespace GameStoreWebAPI.Data
         }
 
         public DbSet<Izdavac> Izdavac { get; set; }
+        public DbSet<Korisnik> Korisnik { get; set; }
     }
 }
