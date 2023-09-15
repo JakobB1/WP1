22da439369ff996e73d9f3f2fdc777c82ed61032
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2023-09-15 12:21:21 +0200

    Dodavanje podataka u bazu

--- zavrsniRad/gamestorewp1.sql
+++ zavrsniRad/gamestorewp1.sql
@@ -20,7 +20,7 @@ create table igra(
 	cijena decimal(18,2) not null,
 	dobnaGranica int not null,
 	datumIzlaska datetime not null,
-	opis varchar(50) not null
+	opis varchar(1000) not null
 );
 
 create table korisnik(
@@ -52,5 +52,20 @@ alter table narudzba add foreign key(korisnik_id) references korisnik(sifra);
 alter table narudzba_igra add foreign key(igra_id) references igra(sifra);
 alter table narudzba_igra add foreign key(narudzba_id) references narudzba(sifra);
 
+select * from izdavac;
 insert into izdavac(naziv,drzava,webStranica)
-values ('Naziv01','Drzava01','Web01');
+values ('Valve','Bellevue, Washington','https://www.valvesoftware.com/en/');
+
+select * from igra;
+insert into igra(naziv,izdavac_id,zanr,cijena,dobnaGranica,datumIzlaska,opis)
+values ('Half-Life',1,'First Person Shooter',8,18,'1998-11-19 00:00:00',
+'Valves debut title blends action and adventure with award-winning technology 
+to create a frighteningly realistic world where players must think to survive');
+
+select * from korisnik;
+insert into korisnik(ime,prezime,oib,email)
+values ('Pero','Peric',12345678911,'pero@mail.com');
+
+select * from narudzba;
+insert into narudzba(korisnik_id,placanje,datum)
+values (1,'Kartica','2023-10-10 00:00:00');
