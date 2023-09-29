2ad45e6db0d32b0f19452c123c69ca3dfa0d5cd9
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2023-09-29 16:47:09 +0200

    Spjanje na backend

--- FrontGame/RWD/js/app.js
+++ FrontGame/RWD/js/app.js
@@ -1,3 +1,8 @@
 $(document).foundation();
 
-
+$.ajax('https://localhost:7179/api/v1/Izdavac',   // request url
+    {
+        success: function (data, status, xhr) {// success callback function
+            console.log(data);
+    }
+});
