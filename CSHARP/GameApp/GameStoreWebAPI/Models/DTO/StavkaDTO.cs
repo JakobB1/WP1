6a684c7d79cadc4f17414a1ce96fa271bb022695
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2023-09-30 11:13:56 +0200

    Workable checkpoint

---
+++ CSHARP/GameApp/GameStoreWebAPI/Models/DTO/StavkaDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace GameStoreWebAPI.Models.DTO
+{
+    public class StavkaDTO
+    {
+        public int Sifra { get; set; }
+        public int Narudzba { get; set; }
+        public int Igra { get; set; }
+    }
+}
