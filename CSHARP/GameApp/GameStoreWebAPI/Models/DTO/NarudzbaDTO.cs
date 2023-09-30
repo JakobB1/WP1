0d5c02761f63e1bd80e398c96aed72d58d7fc2db
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2023-09-30 11:40:28 +0200

    Dodavanje Foreign Key-a na Narudzba,  Dodavanje NarudzbaDTO

---
+++ CSHARP/GameApp/GameStoreWebAPI/Models/DTO/NarudzbaDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace GameStoreWebAPI.Models.DTO
+{
+    public class NarudzbaDTO
+    {
+        public int Sifra { get; set; }
+        public string? Korisnik { get; set; }
+        public string? Placanje { get; set; }
+        public DateTime DatumObracuna { get; set; }
+        public int SifraKorisnik { get; set; }
+    }
+}
--- CSHARP/GameApp/GameStoreWebAPI/Models/Narudzba.cs
+++ CSHARP/GameApp/GameStoreWebAPI/Models/Narudzba.cs
@@ -4,6 +4,8 @@ namespace GameStoreWebAPI.Models
 {
     public class Narudzba : Entitet
     {
+        [ForeignKey("korisnik_id")]
+        public Korisnik? Korisnik { get; set; }
         public string? Placanje { get; set; }
         public DateTime DatumObracuna { get; set; }
     }
