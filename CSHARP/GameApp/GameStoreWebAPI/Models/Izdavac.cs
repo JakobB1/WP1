f8dcc05afced2bed1dd19fd7e47066929e560a66
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2023-10-23 10:47:44 +0200

    Dodavanje validacije da naziv ne može biti broj

--- CSHARP/GameApp/GameStoreWebAPI/Models/Izdavac.cs
+++ CSHARP/GameApp/GameStoreWebAPI/Models/Izdavac.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using GameStoreWebAPI.Validations;
 
 namespace GameStoreWebAPI.Models
 {
     public class Izdavac : Entitet
     {
         [Required]
+        [NazivNeMozeBitiBroj]
         public string? Naziv { get; set; }
         public string? Drzava { get; set; }
         public string? WebStranica { get; set; }
